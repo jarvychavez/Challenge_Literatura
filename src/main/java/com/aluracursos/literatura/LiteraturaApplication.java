bbe41f8dc28cb467337fa0d11a443fecd054f502
jarvychavez <jav.chavez94@gmail.com>
2025-06-12 11:38:55 -0600

    First commit

---
+++ src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
@@ -0,0 +1,15 @@
+package com.aluracursos.literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//JAVIER ALBERTO CHAVEZ CORDOVA
+
+@SpringBootApplication
+public class LiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+}
